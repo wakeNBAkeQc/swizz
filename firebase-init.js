fabc882353d4d9423c16040127251cc17a735d45
wakeNBAkeQc <159395443+wakeNBAkeQc@users.noreply.github.com>
2025-07-14 20:31:58 -0400

    Configure Firebase with provided project details

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,13 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAOurZ3l380i3LMY3-vdAGNRd0KqpYsLp8",
+  authDomain: "zwizz-77189.firebaseapp.com",
+  projectId: "zwizz-77189",
+  storageBucket: "zwizz-77189.firebasestorage.app",
+  messagingSenderId: "840196240615",
+  appId: "1:840196240615:web:41c8ce2be084930bd29e42",
+  measurementId: "G-MXTXZ9S8ZY"
 };
 firebase.initializeApp(firebaseConfig);
+if (firebase.analytics) firebase.analytics();
 window.auth = firebase.auth();
 window.db = firebase.firestore();
